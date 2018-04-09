2051a59b7989ac38421ff0f5a6eb2a6d8f8e97c6
root <root@bitcoissue.com>
2018-04-09 10:10:58 +0900

    a

--- views/board/basic/gallerylist.php
+++ views/board/basic/gallerylist.php
@@ -78,7 +78,7 @@ $record_num = $this->uri->segment($last);
                         <?php if (element('is_admin', $view)) { ?><input type="checkbox" name="chk_post_id[]" value="<?php echo element('post_id', $result); ?>" /><?php } ?>
                         <a href="<?php echo element('post_url', $result); ?>" title="<?php echo html_escape(element('title', $result)); ?>">
                             <figure>
-                                <img src="<?php echo element('thumb_url', $result); ?>" alt="<?php echo html_escape(element('title', $result)); ?>" title="<?php echo html_escape(element('title', $result)); ?>" class="" style="width:<?php echo element('gallery_image_width', element('board', element('list', $view))); ?>px;height:<?php echo element('gallery_image_height', element('board', element('list', $view))); ?>px;" />
+                                <img src="<?php echo element('thumb_url', $result); ?>" alt="<?php echo html_escape(element('title', $result)); ?>" title="<?php echo html_escape(element('title', $result)); ?>" class=""  />
                                 <figcaption>
                                     <h3><?php echo element('num', $result); ?>. <?php echo html_escape(element('title', $result)); ?></h3>
                                     <div class='img_writer'>
