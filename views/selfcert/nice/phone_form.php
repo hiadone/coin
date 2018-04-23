2fe21ea7661a9832c7829b3a805206c46d759887
root <root@bitcoissue.com>
2018-04-23 14:00:19 +0900

    a

--- views/selfcert/nice/phone_form.php
+++ views/selfcert/nice/phone_form.php
@@ -1,11 +1,16 @@
-<!DOCTYPE html>
-<html lang="ko">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ko" xml:lang="ko">
 <head>
-<meta charset="UTF-8">
 <title>휴대폰 본인확인</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Cache-Control" content="No-Cache">
+<meta http-equiv="Pragma" content="No-Cache">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=0,maximum-scale=10">
+<meta name="HandheldFriendly" content="true">
+<meta name="format-detection" content="telephone=no">
 </head>
 <body>
-123123
+
 <form name="form_auth" method="post" target="auth_popup" action="<?php echo element('cert_url', element('niceconfig', $view)); ?>">
 
 <input type="hidden" name="m" value="checkplusSerivce">                     <!-- 필수 데이타로, 누락하시면 안됩니다. -->
@@ -15,8 +20,7 @@
 </form>
 
 <script type="text/javascript">
-    alert(1);
-// document.form_auth.submit();
+document.form_auth.submit();
 </script>
 
 
