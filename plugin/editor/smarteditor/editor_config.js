2d9f54a659d6ad3170f3b872bd0bff4ade49c3ae
root <root@bitcoissue.com>
2018-04-24 16:36:41 +0900

    a

--- plugin/editor/smarteditor/editor_config.js
+++ plugin/editor/smarteditor/editor_config.js
@@ -5,16 +5,42 @@
 
             if( !get_id || $(this).prop("nodeName") != 'TEXTAREA' ) return true;
 
-            nhn.husky.EZCreator.createInIFrame({
+            var ua = navigator.userAgent.toLowerCase();
+            
+            if(ua.indexOf("iphone") < 0 && ua.indexOf("ipad") < 0 && ua.indexOf("ipod") < 0 && ua.indexOf("android") < 0){
+                nhn.husky.EZCreator.createInIFrame({
+                    oAppRef: oEditors,
+                    elPlaceHolder: get_id,
+                    sSkinURI: editor_url+"/SmartEditor2Skin.html",  
+
+                    htParams : {
+                        bUseToolbar : true,                // 툴바 사용 여부 (true:사용/ false:사용하지 않음)
+                        bUseVerticalResizer : true,        // 입력창 크기 조절바 사용 여부 (true:사용/ false:사용하지 않음)
+                        bUseModeChanger : true,            // 모드 탭(Editor | HTML | TEXT) 사용 여부 (true:사용/ false:사용하지 않음)
+                        bSkipXssFilter : true,     // client-side xss filter 무시 여부 (true:사용하지 않음 / 그외:사용)
+                        //aAdditionalFontList : aAdditionalFontSet,     // 추가 글꼴 목록
+                        fOnBeforeUnload : function(){
+                            //alert("완료!");
+                        }
+                    }, //boolean
+                    fOnAppLoad : function(){
+                        //예제 코드
+                        //oEditors.getById["ir1"].exec("PASTE_HTML", ["로딩이 완료된 후에 본문에 삽입되는 text입니다."]);
+                    },
+                    fCreator: "createSEditor2"
+                });
+            } else {
+                nhn.husky.EZCreator.createInIFrame({
                 oAppRef: oEditors,
                 elPlaceHolder: get_id,
-                sSkinURI: editor_url+"/SmartEditor2Skin.html",	
+                sSkinURI: editor_url+"/SmartEditor2Skin.html",  
+
                 htParams : {
-                    bUseToolbar : true,				// 툴바 사용 여부 (true:사용/ false:사용하지 않음)
-                    bUseVerticalResizer : true,		// 입력창 크기 조절바 사용 여부 (true:사용/ false:사용하지 않음)
-                    bUseModeChanger : true,			// 모드 탭(Editor | HTML | TEXT) 사용 여부 (true:사용/ false:사용하지 않음)
-                    bSkipXssFilter : true,		// client-side xss filter 무시 여부 (true:사용하지 않음 / 그외:사용)
-                    //aAdditionalFontList : aAdditionalFontSet,		// 추가 글꼴 목록
+                    bUseToolbar : false,                // 툴바 사용 여부 (true:사용/ false:사용하지 않음)
+                    bUseVerticalResizer : false,        // 입력창 크기 조절바 사용 여부 (true:사용/ false:사용하지 않음)
+                    bUseModeChanger : false,            // 모드 탭(Editor | HTML | TEXT) 사용 여부 (true:사용/ false:사용하지 않음)
+                    bSkipXssFilter : false,     // client-side xss filter 무시 여부 (true:사용하지 않음 / 그외:사용)
+                    //aAdditionalFontList : aAdditionalFontSet,     // 추가 글꼴 목록
                     fOnBeforeUnload : function(){
                         //alert("완료!");
                     }
@@ -25,6 +51,10 @@
                 },
                 fCreator: "createSEditor2"
             });
+            }
+
+
+            
         });
     });
 })(jQuery);
