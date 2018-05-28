3534d52206e07f761af0f9f7c4733ecd1ab81872
root <root@bitcoissue.com>
2018-05-28 19:08:56 +0900

    a

--- application/libraries/Board.php
+++ application/libraries/Board.php
@@ -1003,6 +1003,7 @@ class Board extends CI_Controller
                                 }
                             }
                         } elseif (element('post_link_count', $value)) {
+
                             $this->CI->load->model('Post_link_model');
                             $linkwhere = array(
                                 'post_id' => element('post_id', $value),
@@ -1011,10 +1012,14 @@ class Board extends CI_Controller
                                 ->get('', '', $linkwhere, 1, '', 'pln_id', 'ASC');
                             if ($link && is_array($link)) {
                                 if (element('use_autoplay', $board)) {
+                                    echo "cccc";
                                     $this->CI->load->library('videoplayer');
                                     $link_player .= $this->CI->videoplayer->get_video(prep_url(element('pln_url',element(0,$link))),array('image'=>1) );
                                     $view['view']['latest'][$key]['thumb_url'] = $link_player;
 
+                                } else {
+                                    $thumb_url = get_post_image_url(element('post_content', $value), $image_width, $image_height);
+                                    $view['view']['latest'][$key]['thumb_url'] = $thumb_url ? $thumb_url : thumb_url('', '', $image_width, $image_height);
                                 }
                                 
                             }
