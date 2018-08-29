7000c84d82fce321f8de8b94658bd00d05a2de74
root <root@bitcoissue.com>
2018-08-29 15:27:57 +0900

    b

--- assets/js/common.js
+++ assets/js/common.js
@@ -30,19 +30,7 @@ if (typeof(COMMON_JS) === 'undefined') {
             todayHighlight: true
         });
 
-        $('.datetimepicker').datetimepicker({
-            dateFormat:'yy-mm-dd',
-            monthNamesShort:[ '1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월' ],
-            dayNamesMin:[ '일', '월', '화', '수', '목', '금', '토' ],
-            changeMonth:true,
-            changeYear:true,
-            showMonthAfterYear:true,
-     
-            // timepicker 설정
-            timeFormat:'HH:mm',
-            controlType:'select',
-            oneLine:true,
-        });
+        
     });
 
  
