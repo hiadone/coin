88e77e6ff29b242dab33bdf0188bac0371e54663
root <root@bitcoissue.com>
2020-06-05 10:34:04 +0900

    b

--- application/controllers/Social.php
+++ application/controllers/Social.php
@@ -558,8 +558,8 @@ class Social extends CB_Controller
         }
 
         if ($this->session->userdata('kakao_access_token')) {
-            $url = 'https://kapi.kakao.com/v1/user/me';
-
+            // $url = 'https://kapi.kakao.com/v1/user/me';
+            $url = 'https://kapi.kakao.com/v2/user/me';
             $ch = curl_init();
             curl_setopt ($ch, CURLOPT_URL, $url);
             curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, 0);
